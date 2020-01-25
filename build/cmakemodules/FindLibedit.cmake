0b103ca135a7b65d8087a0c8000cd34adf6b7ac5
Martin Brazeau <martin.brazeau@gmail.com>
2020-01-25 09:04:34 +0000

    remove for renaming with lclibedit

--- build/cmakemodules/FindLibedit.cmake
+++
@@ -1,5 +0,0 @@
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(LIBEDIT REQUIRED libedit)
-message(STATUS "libedit required libraries: ${LIBEDIT_LIBRARIES}")
-
-set(PATH_LIB_EDIT ${LIBEDIT_LIBRARIES})
